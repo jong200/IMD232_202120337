462a08633368e0c99f31761ebd51159ba00142e0
jong200 <140390919+jong200@users.noreply.github.com>
2023-11-06 21:39:15 +0900

    w9

--- src/w7/assignment/step1/script/sketch.js
+++ src/w7/assignment/step1/script/sketch.js
@@ -4,7 +4,7 @@ let infiniteOffset = 80;
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
   colorMode(HSL, 360, 100, 100, 100);
-  background('white');
+  background('salmon'); //배경색상 변경
   traffic = new Traffic();
   for (let n = 0; n < 10; n++) {
     traffic.addVehicle(random(width), random(height));
@@ -12,7 +12,7 @@ function setup() {
 }
 
 function draw() {
-  background('white');
+  background('salmon'); //배경색상 변경
   traffic.run();
 }
 
